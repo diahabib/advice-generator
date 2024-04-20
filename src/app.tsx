5b33f5415cdce3b23a1647f4d4273a884548da7c
diahabib <habibdia45@gmail.com>
2024-04-20 17:22:35 +0000

    update css

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./app.css";
 import axios from "axios";
 import pattern_divider_dektop from "./assets/pattern-divider-desktop.svg";
